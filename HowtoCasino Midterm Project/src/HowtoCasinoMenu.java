afeafc1f6119a8e7654f2636806f7cdd4ab65e52
calvin.farrell <calvin.farrell@MU-C1-11.mullen.pvt.k12.co.us>
2017-09-25 10:30:46 -0600

    Initial Java Commit

--- HowtoCasino Midterm Project/src/HowtoCasinoMenu.java
+++ HowtoCasino Midterm Project/src/HowtoCasinoMenu.java
@@ -462,5 +462,59 @@ public class HowtoCasinoMenu
 					}
 					
 				}
-			
+		public static void keno()
+			{
+				Scanner userInput = new Scanner (System.in);
+				int [] kenoNumbers = new int [20];
+				whilse
+				for (int i = 0; i < 20; i++)
+					{
+						kenoNumbers [i] = (int) (Math.random()*80) + 1; 
+					}
+				System.out.println("Welcome to Keno!");
+				System.out.println("Keno is a number based betting game. You get to pick up to 20 numbers from 1-80.");
+				System.out.println("20 numbers will then be randomely selected.");
+				System.out.println("The more numbers that match the ones you picked, the more you win!.");
+				System.out.println("You have $" + dollars + ".");
+				System.out.println("How much would you like to bet?");
+				int bet = userInput.nextInt();
+				int [] numbersChosen = new int [20];
+				for (int i = 0; i < 20; i++)
+					{
+						System.out.println("Enter a number you want to choose.");
+						numbersChosen [i] = userInput.nextInt();
+					}
+				System.out.println("The winning numbers are:");
+				for (int i = 0; i < 20; i++)
+					{
+						System.out.print(kenoNumbers[i]);
+					}		
+				int numbersThatMatch = 0;
+				for (int i = 0; i < 20; i++)
+					{
+						if (kenoNumbers[i] == numbersChosen[0] || kenoNumbers[i] == numbersChosen[1] || kenoNumbers[i] == numbersChosen[2] || kenoNumbers[i] == numbersChosen[3] || kenoNumbers[i] == numbersChosen[4] || kenoNumbers[i] == numbersChosen[5] || kenoNumbers[i] == numbersChosen[6] || kenoNumbers[i] == numbersChosen[7] || kenoNumbers[i] == numbersChosen[8] || kenoNumbers[i] == numbersChosen[9] || kenoNumbers[i] == numbersChosen[10] || kenoNumbers[i] == numbersChosen[11] || kenoNumbers[i] == numbersChosen[12] || kenoNumbers[i] == numbersChosen[13] || kenoNumbers[i] == numbersChosen[14] || kenoNumbers[i] == numbersChosen[15] || kenoNumbers[i] == numbersChosen[16] || kenoNumbers[i] == numbersChosen[17] || kenoNumbers[i] == numbersChosen[18] || kenoNumbers[i] == numbersChosen[19])
+							{
+								numbersThatMatch++;
+							}
+						else
+							{
+								numbersThatMatch = numbersThatMatch;
+							}
+					}
+				if (numbersThatMatch > 0)
+					{
+						System.out.println("You had " + numbersThatMatch + " winning numbers!");
+						bet = bet*numbersThatMatch;
+						System.out.println("You win $" + bet + "!");
+						dollars = dollars + bet;
+					}
+				else
+					{
+						System.out.println("You had 0 winning numbers.");
+						System.out.println("You lose.");
+						dollars = dollars - bet;
+					}
+				System.out.println("You have $" + dollars + " remaining.");
+			}
 	}
+
--- HowtoCasino Midterm Project/src/Keno.java
+++ HowtoCasino Midterm Project/src/Keno.java
@@ -4,14 +4,55 @@ public class Keno
 	public static void main(String[] args) 
 	{
 		Scanner userInput = new Scanner(System.in);
-		int [] kendoNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80};
+		int dollars = 100;
+		int [] kenoNumbers = new int [20];
+		for (int i = 0; i < 20; i++)
+			{
+				kenoNumbers [i] = (int) (Math.random()*80) + 1; 
+			}
 		System.out.println("Welcome to Keno!");
 		System.out.println("Keno is a number based betting game. You get to pick up to 20 numbers from 1-80.");
 		System.out.println("20 numbers will then be randomely selected.");
 		System.out.println("The more numbers that match the ones you picked, the more you win!.");
-		System.out.println("Enter up to tewnty numbers from 1-80.");
-		int numbersChosen = userInput.nextInt();
-		int [] numbersChosenArray = {numbersChosen};
-		System.out.println("You've chosen " + numbersChosen + ".");
+		System.out.println("You have $" + dollars + ".");
+		System.out.println("How much would you like to bet?");
+		int bet = userInput.nextInt();
+		int [] numbersChosen = new int [20];
+		for (int i = 0; i < 20; i++)
+			{
+				System.out.println("Enter a number you want to choose.");
+				numbersChosen [i] = userInput.nextInt();
+			}
+		System.out.println("The winning numbers are:");
+		for (int i = 0; i < 20; i++)
+			{
+				System.out.print(kenoNumbers[i]);
+			}		
+		int numbersThatMatch = 0;
+		for (int i = 0; i < 20; i++)
+			{
+				if (kenoNumbers[i] == numbersChosen[0] || kenoNumbers[i] == numbersChosen[1] || kenoNumbers[i] == numbersChosen[2] || kenoNumbers[i] == numbersChosen[3] || kenoNumbers[i] == numbersChosen[4] || kenoNumbers[i] == numbersChosen[5] || kenoNumbers[i] == numbersChosen[6] || kenoNumbers[i] == numbersChosen[7] || kenoNumbers[i] == numbersChosen[8] || kenoNumbers[i] == numbersChosen[9] || kenoNumbers[i] == numbersChosen[10] || kenoNumbers[i] == numbersChosen[11] || kenoNumbers[i] == numbersChosen[12] || kenoNumbers[i] == numbersChosen[13] || kenoNumbers[i] == numbersChosen[14] || kenoNumbers[i] == numbersChosen[15] || kenoNumbers[i] == numbersChosen[16] || kenoNumbers[i] == numbersChosen[17] || kenoNumbers[i] == numbersChosen[18] || kenoNumbers[i] == numbersChosen[19])
+					{
+						numbersThatMatch++;
+					}
+				else
+					{
+						numbersThatMatch = numbersThatMatch;
+					}
+			}
+		if (numbersThatMatch > 0)
+			{
+				System.out.println("You had " + numbersThatMatch + " winning numbers!");
+				bet = bet*numbersThatMatch;
+				System.out.println("You win $" + bet + "!");
+				dollars = dollars + bet;
+			}
+		else
+			{
+				System.out.println("You had 0 winning numbers.");
+				System.out.println("You lose.");
+				dollars = dollars - bet;
+			}
+		System.out.println("You have $" + dollars + " remaining.");
 	}
 }
